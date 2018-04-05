1e385f3440730415ff3419ba295116f05ca8c009
net7ton <net7ton@gmail.com>
2018-04-05 17:36:28 +0300

    Initial Commit

---
+++ picstore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  picstore
+//
+//  Created by Aleksandr Kharkov on 05.04.2018.
+//  Copyright © 2018 Oleksandr Kharkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

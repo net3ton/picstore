c330cc716b0342f54dc1e9a291cf74d7d16bd81d
net7ton <net7ton@gmail.com>
2018-07-07 18:09:41 +0300

    album info controller

---
+++ picstore/AlbumController.swift
@@ -0,0 +1,36 @@
+//
+//  AlbumController.swift
+//  picstore
+//
+//  Created by Aleksandr Kharkov on 07/07/2018.
+//  Copyright © 2018 Oleksandr Kharkov. All rights reserved.
+//
+
+import UIKit
+
+enum EAlbumPin {
+    case none
+    case pin4
+}
+
+enum EAlbumHidden {
+    case none
+    case hidden
+}
+
+
+class AlbumController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    //override func didReceiveMemoryWarning() {
+    //    super.didReceiveMemoryWarning()
+    //}
+
+    //override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    //}
+}
--- picstore/MainController.swift
+++ picstore/MainController.swift
@@ -1,64 +1,173 @@
 //
-//  ItemsController.swift
+//  Main2Controller.swift
 //  picstore
 //
-//  Created by Aleksandr Kharkov on 05.04.2018.
+//  Created by Aleksandr Kharkov on 05/07/2018.
 //  Copyright © 2018 Oleksandr Kharkov. All rights reserved.
 //
 
 import UIKit
-import Photos
 
-class MainController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
-    //private var editMode: Bool = false
-    //private var toolbar: UIToolbar?
+class MainController: UIViewController {
+    @IBOutlet weak var itemsView: UICollectionView!
+
+    public var album: AlbumInfo?
+    private var editMode: Bool = false
+    private var itemsDelegate = ItemsDelegate()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        appSettings.load()
-        appGoogleDrive.start()
 
-        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: nil, action: nil)
-        collectionView!.register(ItemViewCell.self, forCellWithReuseIdentifier: ItemViewCell.NAME)
+        if album == nil {
+            appSettings.load()
+            appGoogleDrive.start()
 
-        //let longPress = UILongPressGestureRecognizer(target: self, action: #selector(onLongPressed))
-        //collectionView?.addGestureRecognizer(longPress)
+            album = appData.open()
+        }
 
-        //toolbar = UIToolbar(frame: CGRect(x: 0, y: view.frame.height - 40, width: view.frame.width, height: 40))
+        itemsDelegate.openAlbum = openAlbum
+        itemsDelegate.openItem = openItem
+        itemsDelegate.albumData = album
+
+        itemsView.delegate = itemsDelegate
+        itemsView.dataSource = itemsDelegate
+
+        navigationItem.title = album!.getName()
+        //navigationItem.backBarButtonItem = UIBarButtonItem(title: "Albums", style: .plain, target: nil, action: nil)
+        navigationItem.setHidesBackButton(true, animated: false)
+
+        updateNavigation()
+    }
+
+    public func refresh() {
+        itemsView.reloadData()
+    }
+
+    private func updateNavigation() {
+        if editMode {
+            //let btnBack = UIBarButtonItem(image: UIImage(named: "back"), style: .plain, target: self, action: #selector(onBack))
+            let btnEditDone = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(onEditDone))
+            navigationItem.setRightBarButtonItems([btnEditDone], animated: true)
+            navigationItem.setLeftBarButtonItems([], animated: true)
+        }
+        else {
+            if album!.isRoot() {
+                let btnSettings = UIBarButtonItem(image: UIImage(named: "settings"), style: .plain, target: self, action: #selector(onSettings))
+                navigationItem.setLeftBarButtonItems([btnSettings], animated: true)
+            }
+            else {
+                let btnBack = UIBarButtonItem(image: UIImage(named: "back"), style: .plain, target: self, action: #selector(onBack))
+                let btnAlbumInfo = UIBarButtonItem(image: UIImage(named: "info"), style: .plain, target: self, action: #selector(onAlbumInfo))
+                navigationItem.setLeftBarButtonItems([btnBack], animated: true)
+            }
+
+            let btnAdd = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(onAddItem))
+            let btnEdit = UIBarButtonItem(image: UIImage(named: "props"), style: .plain, target: self, action: #selector(onEdit))
+            navigationItem.setRightBarButtonItems([btnEdit], animated: true)
+        }
+    }
+
+    @objc func onSettings() {
+        let sboard = UIStoryboard(name: "Main", bundle: nil) as UIStoryboard
+        let view = sboard.instantiateViewController(withIdentifier: "settings-main")
+        navigationController?.pushViewController(view, animated: true)
+    }
+
+    @objc func onAddItem() {
+        let sboard = UIStoryboard(name: "Main", bundle: nil) as UIStoryboard
+        let view = sboard.instantiateViewController(withIdentifier: "import-view") as! ImportController
+
+        view.setup(vc: self, album: album!)
+        view.modalPresentationStyle = .popover
+        view.popoverPresentationController?.delegate = view
+        view.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+
+        present(view, animated: true)
+    }
+
+    @objc func onEdit() {
+        editMode = true
+        navigationController?.setToolbarHidden(false, animated: true)
+
+        let btnDelete = UIBarButtonItem(image: UIImage(named: "delete"), style: .plain, target: self, action: nil)
+        let btnCopy = UIBarButtonItem(image: UIImage(named: "copy"), style: .plain, target: self, action: nil)
+        let btnExport = UIBarButtonItem(image: UIImage(named: "export"), style: .plain, target: self, action: nil)
+
+        let sep1 = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let sep2 = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        self.toolbarItems = [btnDelete, sep1, btnCopy, sep2, btnExport]
+
+        updateNavigation()
+    }
+
+    @objc func onEditDone() {
+        editMode = false
+        navigationController?.setToolbarHidden(true, animated: true)
+
+        updateNavigation()
+    }
+
+    @objc func onBack() {
+        navigationController?.popViewController(animated: true)
+    }
+
+    @objc func onAlbumInfo() {
+        let sboard = UIStoryboard(name: "Main", bundle: nil) as UIStoryboard
+        let view = sboard.instantiateViewController(withIdentifier: "settings-album")
+        navigationController?.pushViewController(view, animated: true)
+    }
+    
+    private func openAlbum(album: AlbumObject?) {
+        let sboard = UIStoryboard(name: "Main", bundle: nil) as UIStoryboard
+        let view = sboard.instantiateViewController(withIdentifier: "album-view") as! MainController
+
+        view.album = appData.open(album: album)
+        navigationController?.pushViewController(view, animated: true)
+
+        //curData.open(album: album)
+        //refresh()
+    }
+
+    private func openItem(item: ImageObject?) {
+        let sboard = UIStoryboard(name: "Main", bundle: nil) as UIStoryboard
+        let view = sboard.instantiateViewController(withIdentifier: "item-view") as! ItemController
+
+        view.setup(items: album?.items)
+        navigationController?.pushViewController(view, animated: true)
     }
 
     //override func didReceiveMemoryWarning() {
     //    super.didReceiveMemoryWarning()
     //}
+}
 
-    func refresh() {
-        navigationItem.title = curData.getCurrentAlbumName()
-        collectionView?.reloadData()
-    }
 
-    // MARK: UICollectionViewDataSource
+class ItemsDelegate: NSObject, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    public var albumData: AlbumInfo?
+    public var openAlbum: ((AlbumObject?) -> Void)?
+    public var openItem: ((ImageObject?) -> Void)?
 
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
 
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return curData.getItemsCount()
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return albumData?.getItemsCount() ?? 0
     }
 
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if let album = curData.getAlbum(index: indexPath.row) {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let album = albumData?.getAlbum(index: indexPath.row) {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AlbumViewCell.NAME, for: indexPath) as! AlbumViewCell
             cell.nameLabel.text = album.name
             return cell
         }
 
-        if let item = curData.getItem(index: indexPath.row) {
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ItemViewCell.NAME, for: indexPath) as! ItemViewCell
+        if let item = albumData?.getItem(index: indexPath.row) {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageViewCell.NAME, for: indexPath) as! ImageViewCell
             cell.imageView.image = item.icon
             return cell
         }
-
+        
         return collectionView.dequeueReusableCell(withReuseIdentifier: ParentViewCell.NAME, for: indexPath)
     }
 
@@ -75,54 +184,20 @@ class MainController: UICollectionViewController, UICollectionViewDelegateFlowLa
         return CGSize(width: width/4 - 1, height: width/4 - 1);
     }
 
-    func showItemView() {
-        let sboard = UIStoryboard(name: "Main", bundle: nil) as UIStoryboard
-        let view = sboard.instantiateViewController(withIdentifier: "item-view") as! ItemController
-        
-        //view.setup(category: info)
-        navigationController?.pushViewController(view, animated: true)
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        if let album = curData.getAlbum(index: indexPath.row) {
-            curData.open(album: album)
-            refresh()
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        //if editMode {
+        //curData.select(indexPath.row)
+        //return
+        //}
+
+        if let album = albumData?.getAlbum(index: indexPath.row) {
+            openAlbum?(album)
             return
         }
 
-        if let item = curData.getItem(index: indexPath.row) {
-            showItemView()
+        if let item = albumData?.getItem(index: indexPath.row) {
+            openItem?(item)
             return
-        }
-
-        curData.openParentAlbum()
-        refresh()
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
-        //let cell = collectionView.cellForItem(at: indexPath)
-        //cell?.backgroundColor = UIColor.brown
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath) {
-        //let cell = collectionView.cellForItem(at: indexPath)
-        //cell?.backgroundColor = UIColor.white
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "add_item" {
-            if let importVC = segue.destination as? ImportController {
-                importVC.popoverPresentationController?.delegate = importVC
-                importVC.root = self
-            }
         }
     }
 }

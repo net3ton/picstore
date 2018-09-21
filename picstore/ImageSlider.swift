be78db6de2089628d62823482f685c93d1e344a9
net7ton <net7ton@gmail.com>
2018-09-21 13:10:17 +0300

    image slider control update

--- picstore/ImageSlider.swift
+++ picstore/ImageSlider.swift
@@ -8,12 +8,154 @@
 
 import UIKit
 
-class ImageSliderItem: UIImageView {
+class ImageSliderItem: UIScrollView, UIScrollViewDelegate, UIGestureRecognizerDelegate {
     /// representing page
     public var page: Int = -1
     /// position in scrollview
     public var pos: Int = 0
+
+    public var onImageTap: (() -> Void)?
+
+    private var imageView = UIImageView()
+    private var tapDouble: UITapGestureRecognizer!
+    private var tapOnce: UITapGestureRecognizer!
+
+    public var image: UIImage? {
+        set {
+            imageView.image = newValue
+            initSize()
+        }
+        get {
+            return imageView.image
+        }
+    }
+
+    init(position: Int) {
+        super.init(frame: CGRect.null)
+
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.isUserInteractionEnabled = true
+        addSubview(imageView)
+
+        self.delegate = self
+        self.showsVerticalScrollIndicator = false
+        self.showsHorizontalScrollIndicator = false
+        self.minimumZoomScale = 1.0
+        self.maximumZoomScale = 2.0
+        self.pos = position
+
+        tapDouble = UITapGestureRecognizer(target: self, action: #selector(onDoubleTap))
+        tapDouble.numberOfTapsRequired = 2
+        tapDouble.delegate = self
+        imageView.addGestureRecognizer(tapDouble)
+
+        tapOnce = UITapGestureRecognizer(target: self, action: #selector(onOneTap))
+        tapOnce.delegate = self
+        imageView.addGestureRecognizer(tapOnce)
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public func initSize() {
+        let size = calculateContentSize()
+
+        if let image = imageView.image {
+            self.maximumZoomScale = image.size.width / size.width
+        }
+        else {
+            self.maximumZoomScale = 1.0
+        }
+        
+        self.setZoomScale(1.0, animated: false)
+        self.minimumZoomScale = 1.0
+
+        imageView.frame = CGRect(origin: CGPoint.zero, size: size)
+        centerContent()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        centerContent()
+    }
+
+    private func calculateContentSize() -> CGSize {
+        guard let image = imageView.image else {
+            return self.frame.size
+        }
+
+        let picRatio = image.size.width / image.size.height
+        let screenRatio = self.frame.width / self.frame.height
+        var viewSize: CGSize!
+
+        if picRatio > screenRatio {
+            viewSize = CGSize(width: self.frame.width, height: self.frame.width / picRatio)
+        } else {
+            viewSize = CGSize(width: self.frame.height * picRatio, height: self.frame.height)
+        }
+
+        return viewSize
+    }
+
+    private func centerContent() {
+        let intendHorizon = max(0, (self.frame.width - imageView.frame.width) / 2)
+        let intendVertical = max(0, (self.frame.height - imageView.frame.height) / 2)
+        self.contentInset = UIEdgeInsets(top: intendVertical, left: intendHorizon, bottom: intendVertical, right: intendHorizon)
+    }
+
+    private func isZoomed() -> Bool {
+        return self.zoomScale > self.minimumZoomScale
+    }
+    
+    @objc func onDoubleTap(_ sender: UITapGestureRecognizer) {
+        if isZoomed() {
+            self.setZoomScale(self.minimumZoomScale, animated: true)
+            return
+        }
+
+        let pos = sender.location(in: self.imageView)
+        let zoomRect = zoomRectForScale(scale: maximumZoomScale, center: pos)
+        zoom(to: zoomRect, animated: true)
+    }
+    
+    private func zoomRectForScale(scale: CGFloat, center: CGPoint) -> CGRect {
+        var zoomRect = CGRect.zero
+
+        zoomRect.size.height = self.imageView.frame.size.height / scale
+        zoomRect.size.width  = self.imageView.frame.size.width / scale
+        zoomRect.origin.x = center.x - (zoomRect.size.width / 2.0)
+        zoomRect.origin.y = center.y - (zoomRect.size.height / 2.0)
+
+        return zoomRect
+    }
+
+    @objc func onOneTap() {
+        onImageTap?()
+    }
+    
+    // MARK: UIScrollViewDelegate
+
+    internal func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return self.imageView
+    }
+
+    internal func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        centerContent()
+    }
+
+    // MARK: UIGestureRecognizerDelegate
+
+    internal func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailBy otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        if gestureRecognizer == tapDouble && otherGestureRecognizer == tapOnce {
+            return true
+        }
+
+        return false
+    }
 }
+
 
 class ImageSlider: UIView {
     private let scrollView = UIScrollView()
@@ -27,6 +169,15 @@ class ImageSlider: UIView {
     public var pagesCount = 5
 
     public var imageForPage: ((_ page: Int) -> UIImage?)?
+    public var onImageTap: (() -> Void)?
+    {
+        didSet {
+            for view in scrollView.subviews {
+                let iview = view as! ImageSliderItem
+                iview.onImageTap = onImageTap
+            }
+        }
+    }
 
     override public init(frame: CGRect) {
         super.init(frame: frame)
@@ -47,17 +198,13 @@ class ImageSlider: UIView {
         scrollView.bounces = true
         scrollView.showsHorizontalScrollIndicator = false
         scrollView.showsVerticalScrollIndicator = false
-        scrollView.autoresizingMask = self.autoresizingMask
         addSubview(scrollView)
 
         /// current view + advance views in both sides
         let viewsCount = ADVANCE * 2 + 1
 
         for i in 0..<viewsCount {
-            let view = ImageSliderItem()
-            view.contentMode = .scaleAspectFit
-            view.pos = i
-            scrollView.addSubview(view)
+            scrollView.addSubview(ImageSliderItem(position: i))
         }
 
         // setup timer
@@ -66,21 +213,15 @@ class ImageSlider: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
 
-        print("layoutSubviews")
         // fixes the case when automaticallyAdjustsScrollViewInsets on parenting view controller is set to true
-        scrollView.contentInset = UIEdgeInsets.zero
+        //scrollView.contentInset = UIEdgeInsets.zero
         layoutScrollView()
     }
 
     private func layoutScrollView() {
-        let size = scrollView.frame.size
+        let size = self.bounds.size
         let count = scrollView.subviews.count
-
-        //UIView.animate(withDuration: 0.4) {
-        //    self.scrollView.frame = self.bounds
-        //    self.scrollView.contentSize = CGSize(width: size.width * CGFloat(count), height: size.height)
-        //}
-
+        
         scrollView.frame = self.bounds
         scrollView.contentSize = CGSize(width: size.width * CGFloat(count), height: size.height)
 
@@ -90,25 +231,27 @@ class ImageSlider: UIView {
     private func layoutViews() {
         let size = scrollView.frame.size
 
-        for view in scrollView.subviews {
-            let iview = view as! ImageSliderItem
-            iview.frame = CGRect(origin: CGPoint(x: size.width * CGFloat(iview.pos), y: 0), size: size)
-
+        for case let view as ImageSliderItem in scrollView.subviews {
             /// current page should be in ADVANCE position
-            let page = getNormalizedPage((iview.pos - ADVANCE) + pageCurrent)
-            if iview.page != page {
-                iview.page = page
-                iview.image = imageForPage?(page)
+            let page = getNormalizedPage((view.pos - ADVANCE) + pageCurrent)
+            if view.page != page {
+                view.page = page
+                view.image = imageForPage?(page)
             }
+
+            view.frame = CGRect(origin: CGPoint(x: size.width * CGFloat(view.pos), y: 0), size: size)
         }
 
         setScrollViewPosition(ADVANCE)
+        
+        for case let view as ImageSliderItem in scrollView.subviews {
+            view.initSize()
+        }
     }
 
     private func moveVeiws(with delta: Int) {
-        for view in scrollView.subviews {
-            let iview = view as! ImageSliderItem
-            iview.pos = getNormalizedPosition(iview.pos - delta)
+        for case let view as ImageSliderItem in scrollView.subviews {
+            view.pos = getNormalizedPosition(view.pos - delta)
         }
 
         layoutViews()
@@ -162,5 +305,20 @@ extension ImageSlider: UIScrollViewDelegate {
             pageCurrent = getNormalizedPage(pageCurrent + delta)
             moveVeiws(with: delta)
         }
+    }
+}
+
+
+extension ImageSlider {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
     }
 }

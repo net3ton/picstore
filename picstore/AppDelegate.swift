b351787eec545e9484a5b69e76765973dbd26182
net7ton <net7ton@gmail.com>
2019-01-09 13:50:07 +0300

    nothing

--- picstore/AppDelegate.swift
+++ picstore/AppDelegate.swift
@@ -39,20 +39,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-    /*
-    func getTopViewController(root: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
-        if let navigationController = root as? UINavigationController {
-            return getTopViewController(root: navigationController.visibleViewController)
-        }
-        if let tabbarController = root as? UITabBarController {
-            return getTopViewController(root: tabbarController.selectedViewController)
-        }
-        if let presentedController = root?.presentedViewController {
-            return getTopViewController(root: presentedController)
-        }
-
-        return root
-    }
-    */
 }
